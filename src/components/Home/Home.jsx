eb60bddab05d7e4f1fd36253e3528a9fd78d0603
Hussein Khaleefah <husseinosamah2@gmail.com>
2023-03-09 16:50:01 +0100

    alisaadart

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,5 +3,24 @@
 import React from "react";
 import "./Home.css";
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div id="app">
+        <div class="homeTitle">
+          <div class="homeTitle-inner">
+            <div class="cafe">
+              <div class="cafe-inner">Ali Saad</div>
+            </div>
+            <div class="mozart">
+              <div class="mozart-inner">I'm an Interior Designer</div>
+            </div>
+          </div>
+        </div>
+
+        <div class="image">
+          <img src="/assets/images/bg.JPG" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 }
