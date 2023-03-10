27c07e0d7a5d03f503ff12f1f3e3b61db3481361
Hussein Khaleefah <husseinosamah2@gmail.com>
2023-03-10 10:59:13 +0100

    alisaadart

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,7 +9,13 @@ export default function Footer() {
       <div className="container">
         <footer className="d-flex flex-wrap justify-content-between align-items-center py-1">
           <div className="col-md-4 d-flex align-items-center">
-            <span className="">© 2023 Ali Saad</span>
+            <a
+              className="Powered"
+              href="https://instagram.com/jh_developers?igshid=YmMyMTA2M2Y="
+              target="_blank"
+            >
+              <span className="">© Powered by J&H developers.</span>
+            </a>
           </div>
 
           <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
