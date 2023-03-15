0e95a10b5d0137dba5202b90e4924abcd4ef66f4
Hussein Khaleefah <husseinosamah2@gmail.com>
2023-03-15 20:59:06 +0100

    alisaadart

--- src/components/Projects/RestaurantsCafes/RestaurantsCafes.jsx
+++ src/components/Projects/RestaurantsCafes/RestaurantsCafes.jsx
@@ -15,7 +15,13 @@ export default function RestaurantsCafes() {
 
   useEffect(() => {
     fetch(
-      "http://alisaad.jhdevelopers.com/api/cases-restaurantscafes-projects/$2a$12$5ZRbXc9SCjbQiMkA8uWjZeRM96utZI3CmksHJoP7GWtosi0EGYGfq"
+      "http://alisaad.jhdevelopers.com/api/cases-restaurantscafes-projects/$2a$12$5ZRbXc9SCjbQiMkA8uWjZeRM96utZI3CmksHJoP7GWtosi0EGYGfq",
+      {
+        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
     )
       .then((response) => response.json())
       .then((data) => {
